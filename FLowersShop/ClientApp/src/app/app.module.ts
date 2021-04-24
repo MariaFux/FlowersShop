9aa3fbf47cd23f9d1b227cfbf0fcf52e081440b8
мария пугачёва <mawytochka99@mail.ru>
2021-04-24 20:01:01 +0300

    added delivery block

--- FLowersShop/ClientApp/src/app/app.module.ts
+++ FLowersShop/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -35,6 +35,7 @@ import { CartSummaryComponent } from './components/cart/cart-summary/cart-summar
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'cart', component: CartComponent },
