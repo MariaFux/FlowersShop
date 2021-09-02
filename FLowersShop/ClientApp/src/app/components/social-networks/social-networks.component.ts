401b6153566d0615bacf62117cdb32675efdd83d
мария пугачёва <mawytochka99@mail.ru>
2021-09-02 19:30:32 +0300

    added social networks component

---
+++ FLowersShop/ClientApp/src/app/components/social-networks/social-networks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-networks',
+  templateUrl: './social-networks.component.html',
+  styleUrls: ['./social-networks.component.css']
+})
+export class SocialNetworksComponent {
+  
+  constructor() {
+  }
+}
