874fdd5c8fd452db072f7d7b0dff64490d15999b
мария пугачёва <mawytochka99@mail.ru>
2021-09-03 18:54:48 +0300

    changed address string

--- FLowersShop/ClientApp/src/app/components/home/nav-menu/nav-menu.component.ts
+++ FLowersShop/ClientApp/src/app/components/home/nav-menu/nav-menu.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
   public address: string = "Витебск, ул. Смоленская 5A, пом. 28";
-  private url: string = "https://www.google.com/maps/place/%D0%92%D0%B8%D1%82%D0%B5%D0%B1%D1%81%D0%BA/@55.1939583,30.1248546,12z/data=!3m1!4b1!4m5!3m4!1s0x46c573e3fc7c7be7:0x430637d0624afff4!8m2!3d55.1926809!4d30.206359"
+  private url: string = "https://www.google.com/maps/place/%D0%A6%D0%B2%D0%B5%D1%82%D0%BE%D1%87%D0%BD%D1%8B%D0%B9+%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD+%C2%AB%D0%90%D1%81%D1%82%D1%80%D0%B0%C2%BB/@55.1811133,30.2301514,16.25z/data=!4m9!1m2!2m1!1z0LDRgdGC0YDQsA!3m5!1s0x0:0x93e74ffae15c81ef!8m2!3d55.1823006!4d30.2310356!15sCgrQsNGB0YLRgNCwkgEHZmxvcmlzdA"
 
   onAddressClick() {
     window.open(this.url, '_blank');
