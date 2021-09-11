ae52a6497f84c03550b4ced6fdf8066a2f89eaee
мария пугачёва <mawytochka99@mail.ru>
2021-09-11 17:17:30 +0300

    added categories seeding

--- FlowersShop.DAL/DataContext/FlowersShopDbContext.cs
+++ FlowersShop.DAL/DataContext/FlowersShopDbContext.cs
@@ -21,6 +21,14 @@ namespace FlowersShop.DAL.DataContext
                 .HasOne(p => p.Category)
                 .WithMany(c => c.Products)
                 .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Цветы", Description = "Широкий ассортимент цветов!", PhotoPath = "/assets/images/flowers.png" },
+                new Category { Id = 2, Name = "Букеты", Description = "Сборные и свадебные букеты!", PhotoPath = "/assets/images/bouquet.png" },
+                new Category { Id = 3, Name = "Композиции", Description = "Композиции в шляпке, на оазисе, в корзинке!", PhotoPath = "/assets/images/composition.png" },
+                new Category { Id = 4, Name = "Подарки", Description = "Гелиевые шары, открытки!", PhotoPath = "/assets/images/balloons.png" },
+                new Category { Id = 5, Name = "Пиньяты", Description = "Пиньята на детский праздник сможет порадовать вашего ребенка!", PhotoPath = "/assets/images/pinata.png" },
+                new Category { Id = 6, Name = "Свечи", Description = "Свечи ручной работы на любой вкус и цвет!", PhotoPath = "/assets/images/candle.png" });
         }
     }
 }
---
+++ FlowersShop.DAL/Migrations/20210911141211_CategoriesSeeding.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlowersShop.DAL.Migrations
+{
+    public partial class CategoriesSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, "Широкий ассортимент цветов!", "Цветы", "/assets/images/flowers.png" },
+                    { 2, "Сборные и свадебные букеты!", "Букеты", "/assets/images/bouquet.png" },
+                    { 3, "Композиции в шляпке, на оазисе, в корзинке!", "Композиции", "/assets/images/composition.png" },
+                    { 4, "Гелиевые шары, открытки!", "Подарки", "/assets/images/balloons.png" },
+                    { 5, "Пиньята на детский праздник сможет порадовать вашего ребенка!", "Пиньяты", "/assets/images/pinata.png" },
+                    { 6, "Свечи ручной работы на любой вкус и цвет!", "Свечи", "/assets/images/candle.png" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
