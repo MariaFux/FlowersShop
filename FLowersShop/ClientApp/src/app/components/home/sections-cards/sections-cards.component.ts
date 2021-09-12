d0d1fa45e42a4f4c33a4b3d40b4796d64d519932
мария пугачёва <mawytochka99@mail.ru>
2021-09-12 19:00:31 +0300

    old cards data is replaced with data received from the db, generating cards via ngFor

--- FLowersShop/ClientApp/src/app/components/home/sections-cards/sections-cards.component.ts
+++ FLowersShop/ClientApp/src/app/components/home/sections-cards/sections-cards.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../../models/category.model';
+import { CategoryService } from '../../../services/category.service';
 
 @Component({
   selector: 'app-sections-cards',
   templateUrl: './sections-cards.component.html',
   styleUrls: ['./sections-cards.component.css']
 })
-export class SectionsCardsComponent {
+export class SectionsCardsComponent implements OnInit {
+  constructor(private categoryService: CategoryService) {
+  }
+
+  categories: Category;
+
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    })
+  }
 }
