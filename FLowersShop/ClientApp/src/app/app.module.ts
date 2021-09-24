8ce04117b0cbb735233e56c862127e5ce518bc31
мария пугачёва <mawytochka99@mail.ru>
2021-09-24 11:05:02 +0300

    cleaned the app module from components that aren't used

--- FLowersShop/ClientApp/src/app/app.module.ts
+++ FLowersShop/ClientApp/src/app/app.module.ts
@@ -7,16 +7,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/home/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
-import { ViewComponent } from './components/view/view.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ImageGalleryComponent } from './components/home/image-gallery/image-gallery.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SectionsCardsComponent } from './components/home/sections-cards/sections-cards.component';
-import { NewArrivalComponent } from './components/home/new-arrival/new-arrival.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ScreenHeaderComponent } from './components/screen-header/screen-header.component';
-import { CartSummaryComponent } from './components/cart/cart-summary/cart-summary.component';
-import { ProfileComponent } from './components/profile/profile.component';
 import { SearchComponent } from './components/search/search.component';
 import { SocialNetworksComponent } from './components/social-networks/social-networks.component';
 import { DeliveryPayComponent } from './components/delivery-pay/delivery-pay.component';
@@ -29,15 +23,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CartComponent,
-    ViewComponent,
-    ImageGalleryComponent,
     SectionsCardsComponent,
-    NewArrivalComponent,
     FooterComponent,
     ScreenHeaderComponent,
-    CartSummaryComponent,
-    ProfileComponent,
     SearchComponent,
     SocialNetworksComponent,
     DeliveryPayComponent,
@@ -54,8 +42,6 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatSelectModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'view', component: ViewComponent },
-      { path: 'profile', component: ProfileComponent },
       { path: 'search', component: SearchComponent },
       { path: 'delivery-pay', component: DeliveryPayComponent },
       { path: 'contacts', component: ContactsComponent },
--- FLowersShop/ClientApp/src/app/components/view/view.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'view',
-  templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
-})
-export class ViewComponent {
-}
